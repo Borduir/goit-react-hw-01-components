ba7884b5154e2d5474b327fcbcfeb33637da43fb
Denis Smolianyi <denis.smolyanoj@gmail.com>
2022-10-28 19:02:23 +0300

    first task completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
-import CreateUser from './CreateUser'
-import user from '../render-files/user.json'
+import Profile from './user/Profile'
+import user from './user/user.json'
 
 const { username, tag, location, avatar, stats } = user 
 export const App = () => {
   return (
-    <CreateUser avatar ={avatar} username ={username} location = {location} stats = {stats} tag={tag} ></CreateUser>
+    <Profile avatar = {avatar} username = {username} location = {location} stats = {stats} tag = {tag} ></Profile>
   );
 };
