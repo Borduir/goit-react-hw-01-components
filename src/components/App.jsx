fdc717f25d3d44f2d0287723cea246733a841919
Denis Smolianyi <denis.smolyanoj@gmail.com>
2022-10-29 14:42:52 +0300

    hw completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,15 @@
-import Profile from './user/Profile'
-import Statistics from '../components/Statistics/Stats'
-import user from './user/user.json'
 import { Fragment } from 'react';
+
+import Profile from './user/Profile'
+import user from './user/user.json'
+import Statistics from '../components/Statistics/Stats'
 import dataList from '../components/Statistics/data.json'
 import Friendlist from './Friendlist/Friendlist'
 import friends from '../components/Friendlist/friends.json'
+import TransactionHistory from './Transactions/TransactionHistory'
+import items from '../components/Transactions/transactions.json'
+
+
 
 const { username, tag, location, avatar, stats } = user 
 
@@ -12,7 +17,8 @@ export const App = () => {
   return (<Fragment>
     <Profile avatar={avatar} username={username} location={location} stats={stats} tag={tag} ></Profile>
     <Statistics dataList={dataList} statTitle={"Upload title"}></Statistics>
-        <Friendlist friends={friends} ></Friendlist>
+    <Friendlist friends={friends} ></Friendlist>
+    <TransactionHistory items ={items}></TransactionHistory>
   </Fragment>
 
     );
---
+++ src/components/Transactions/TransactionHistory.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import css from './Transactions.module.css'
+
+export default function TransactionHistory({ items }) {
+  return (
+      <table className={css.transactionHistory}>
+  <thead>
+    <tr>
+      <th className={css.tabletHead}>Type</th>
+      <th className={css.tabletHead}>Amount</th>
+      <th className={css.tabletHead}>Currency</th>
+    </tr>
+  </thead>
+  <tbody>
+        {items.map(item => (
+      <tr key={item.id} className={css.tabletRow}>
+      <td className={css.tabletCell}>{item.type}</td>
+      <td className={css.tabletCell}>{item.amount}</td>
+      <td className={css.tabletCell}>{item.currency}</td>
+    </tr>)
+    )}
+  </tbody>
+</table>
+    )
+}
+
+TransactionHistory.propTypes = {
+  items: PropTypes.array.isRequired
+}
